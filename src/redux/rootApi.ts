0ccfea04034f3c608feec6243abd839919a3507b
Yaroslav Nazarenko <yinazarenkoo@gmail.com>
2023-05-01 21:07:58 +0300

    api implementation

--- src/redux/rootApi.ts
+++ src/redux/rootApi.ts
@@ -1,22 +1,38 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import type { Location } from '../shared/types'
+import type { Forecast, Location, forecastResponse } from '../shared/types'
 
 const baseQuery = fetchBaseQuery({
   baseUrl: 'https://api.open-meteo.com/v1'
 })
 
 const getEndpoint = ({latitude, longitude}: Location) => {
-  return `forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`
+  return `forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,cloudcover,windspeed_10m,precipitation_probability`
 } 
+
+const transformForecastFormat = (response: forecastResponse) => {
+  console.log(response);
+  
+  return response.hourly.time.reduce<Forecast[]>((acc, _i, index) => {
+    acc.push({
+      date: response.hourly.time[index],
+      temperature: response.hourly.temperature_2m[index],
+      cloudcover: response.hourly.cloudcover[index],
+      windspeed: response.hourly.windspeed_10m[index],
+      precipitationProbability: response.hourly.precipitation_probability[index]
+    })
+    return acc
+  }, [])
+}
 
 export const rootApi = createApi({
   baseQuery,
   reducerPath: 'api',
   endpoints: build => ({
-    getForecast: build.query<unknown, Location>({
-      query: (Location) => getEndpoint(Location)
+    getForecast: build.query<Forecast[], Location>({
+      query: (Location) => getEndpoint(Location),
+      transformResponse: transformForecastFormat
     }),
   }),
 })
 
-export const { useGetForecastQuery } = rootApi
+export const { useGetForecastQuery, useLazyGetForecastQuery } = rootApi
--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -2,3 +2,33 @@ export interface Location {
   latitude: number,
   longitude: number
 }
+export interface forecastResponse {
+  latitude: number,
+  longitude: number,
+  generationtime_ms: number,
+  utc_offset_seconds: number,
+  timezone: string,
+  timezone_abbreviation: string,
+  elevation: number,
+  hourly_units: HourlyUnits,
+  hourly: {
+    time: string[]
+    temperature_2m: number[]
+    cloudcover: number[]
+    windspeed_10m: number[],
+    precipitation_probability: number[]
+  }
+}
+export interface Forecast {
+  date: string,
+  temperature: number
+  cloudcover: number
+  windspeed: number
+  precipitationProbability: number
+}
+
+
+interface HourlyUnits {
+ time: string,
+ temperature_2m: string
+}
